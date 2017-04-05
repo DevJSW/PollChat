8411bffc34846d16e9c591b2f31dde716c1a8b27
DevJSW <johnwasike0@gmail.com>
2017-04-05 09:47:55 +0300

    commiting changes

--- app/src/main/java/com/pollchat/pollchat/MainActivity.java
+++ app/src/main/java/com/pollchat/pollchat/MainActivity.java
@@ -1,36 +1,65 @@
 package com.pollchat.pollchat;
 
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
     private String post_name = null;
+    private TextView mNoPostTxt;
     SwipeRefreshLayout mSwipeRefreshLayout;
+    private ImageView mCallBtn, mImg;
     private DatabaseReference mDatabaseUsers;
+    private DatabaseReference mDatabaseComment;
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private ProgressBar mProgressBar;
+    private RecyclerView mLettersList;
+    private FirebaseUser mCurrentUser;
+    private StorageReference mStorage;
+    private Uri mImageUri = null;
+    private static int GALLERY_REQUEST =1;
+    private Boolean mProcessLike = false;
+    private DatabaseReference mDatabaseLike;
+
+    private Query mQueryLetters;
+    private Query mQueryComments;
+    private Query mQueryLikes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +68,18 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        mImg = (ImageView) findViewById(R.id.post_userimg);
+       // mNoPostTxt = (TextView) findViewById(R.id.noPostTxt);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+               // Intent cardonClick = new Intent(MainActivity.this, PostActivity.class);
+                //startActivity(cardonClick);
+
             }
         });
 
@@ -54,8 +89,36 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Refresh items
+                refreshItems();
+            }
+        });
+
+        //final String user_id = mAuth.getCurrentUser().getUid();
         mAuth = FirebaseAuth.getInstance();
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Letters");
+        mDatabaseComment = FirebaseDatabase.getInstance().getReference().child("Comments");
+        mDatabaseLike = FirebaseDatabase.getInstance().getReference().child("Likes");
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar2);
+        mLettersList = (RecyclerView) findViewById(R.id.letters_list);
+        mLettersList.setLayoutManager(new LinearLayoutManager(this));
+        mLettersList.setHasFixedSize(true);
+
+        mDatabase.keepSynced(true);
+        mDatabaseUsers.keepSynced(true);
+        mDatabaseLike.keepSynced(true);
+        mDatabase.keepSynced(true);
+
+
 
         mDatabaseUsers.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
             @Override
@@ -80,9 +143,82 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+        checkUserExists();
+        checkForNetwork();
+
     }
+
+
+    private void checkForNetwork() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo ninfo = cm.getActiveNetworkInfo();
+
+        if (ninfo != null && ninfo.isConnected()) {
+
+
+        } else {
+
+
+            Toast.makeText(MainActivity.this, "You are not connected to Internet... Please Enable Internet!", Toast.LENGTH_LONG)
+                    .show();
+
+        }
+    }
+
+    private void checkUserExists() {
+
+        mProgressBar.setVisibility(View.VISIBLE);
+        final String user_id = mAuth.getCurrentUser().getUid();
+
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                if (!dataSnapshot.hasChild(user_id)) {
+
+                    mProgressBar.setVisibility(View.GONE);
+
+                  //  Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                   // intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                  //  startActivity(intent);
+                 //   finish();
+
+                }else {
+
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                mProgressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    void refreshItems() {
+        // Load items
+        // ...
+
+        // Load complete
+        onItemsLoadComplete();
+    }
+
+    void onItemsLoadComplete() {
+        // Update the adapter and notify data set changed
+        // ...
+
+        // Stop refresh animation
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+
 
     @Override
     public void onBackPressed() {
@@ -125,6 +261,10 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_profile) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
 
